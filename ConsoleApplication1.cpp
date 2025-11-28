07f74391b7d5fbc40f1c11e02fee9605e8bb7e3c
Ксения <maslovaks1007@gmail.com>
2025-11-28 08:51:09 +0400

    комит 3

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -11,8 +11,6 @@ void main()
     printf("Дата: 02.09.2025\n");
     printf("Гpyппа: ИСЭбд-11-2025\n");
     printf("\n");
-    printf("Сергеевна\n");
-    printf("Ксения\n");
     printf("\n");
     printf("\n");
     printf("Маслова\n");
