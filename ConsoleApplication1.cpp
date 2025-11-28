c43d80933334b64ec618ce882698e04df44b62d3
Ксения <maslovaks1007@gmail.com>
2025-11-28 08:49:55 +0400

    комит 1

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+void main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    printf("Лабораторная работа №1\n");
+    printf("Предмет: Основы алгоритмизации и программирование\n");
+    printf("Дата: 02.09.2025\n");
+    printf("Гpyппа: ИСЭбд-11-2025\n");
+    printf("\n");
+    printf("Сергеевна\n");
+    printf("Ксения\n");
+    printf("Маслова\n");
+    printf("\n");
+    printf("\n");
+    printf("Маслова\n");
+    printf("Ксения\n");
+    printf("Сергеевна\n");
+    printf("\n");
+    printf("Гpyппа: ИСЭбд-11-2025\n");
+    printf("Дата: 02.09.2025\n");
+    printf("Предмет: Основы алгоритмизации и программирование\n");
+    printf("Лабораторная работа №1\n");
+}
